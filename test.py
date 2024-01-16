89be0ac636b7b58441c766588df575e83bc4d75b
choochoo83 <ilovemaxis@yahoo.com>
2024-01-16 15:13:18 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys, os, time, re
+sys.path.append(r'C:\Users\Public\Documents\LeCroy\SAS4 Protocol Suite\API\SDK\Bin')
+import TLSASAPI
+
+
+def opentrace(trace_file):
+	open_trace = TLSASAPI.OpenFile(trace_file)
+	total_packet_in_trace = open_trace.GetCount()
+	if total_packet_in_trace != 0:
+		return("Passed")
+	else:
+		return("Failed")
+	
+print(opentrace("C:\\Users\\Public\\Documents\\LeCroy\\SAS4 Protocol Suite\\Examples\\Traces\\AfterJammer Modified SCSICommand.sxt"))
