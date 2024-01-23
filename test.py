32d09a4f619b6f92370b8243b19a4c5a3bfb44e6
choochoo83 <155510053+choochoo83@users.noreply.github.com>
2024-01-23 11:53:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -32,3 +32,5 @@ print("Hi Jenkins - is webhook triggered the build (with token credential select
 
 print("Hi Jenkins - is it running every minutes with polling SCM setting?")
 
+print("Hi Jenkins - did you run automatically via webhook trigger?")
+
